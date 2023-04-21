780d49c287fd0a2ace0cec8550a3efba521bbbdb
rohitjadhav016 <jrohit016@gmail.com>
2023-04-22 00:15:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const[movies, setMovies] = useState([]);
+    //state for search functionality
+    const[searchKeyword, setSearchKeyword] = useState('');
+
+    //api key
+    const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=1f16786f`;
+
+    const searchMovies = async (title) => {
+        const response =  await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+        
+    }
+
+    useEffect(() => {
+        searchMovies('SpiderMan');
+    }, []);
+
+    return(
+        <div className='app'>
+        <h1> Movie World </h1>
+        <div className='search'>
+            <input placeholder='Search for a movie' value={searchKeyword} onChange={(e) => setSearchKeyword(e.target.value)}  />
+            <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchKeyword)}/>
+        </div>
+        {
+            movies.length > 0
+            ? (
+                <div className='container'>
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+            ))}
+                </div>
+            )
+            : (
+                <div>
+                    <h3> No movies found </h3>
+                </div>
+            )
+        }
+        </div>
+    );
 }
 
 export default App;
